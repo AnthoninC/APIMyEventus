16932789971ce15f72742ad2c1edd36f95cd160e
AnthoninC <79632139+AnthoninC@users.noreply.github.com>
2021-11-22 08:50:44 +0100

    Creation des méthodes de l'API

--- MyEventusApi/src/main/java/controller/EvenementController.java
+++ MyEventusApi/src/main/java/controller/EvenementController.java
@@ -3,6 +3,8 @@ package controller;
 import java.util.Date;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -27,81 +29,31 @@ public class EvenementController {
 	
     @GetMapping("/all")
     public String all() {
-        return "Liste de tous les event";
+        List<Evenement> events = jdbcTemplate.query("SELECT * FROM EVENEMENT", new EvenementRowMapper());
+    	return new Gson().toJson(events);
     }
-    
-    /*
-    @GetMapping("/allevents")
-    public String listAll(Model model) {
-        List<Evenement> listStudents = eventRepo.findAll();
-        model.addAttribute("listStudents", listStudents);
-           
-        return "allevents";
-    }
-    */
-    
-    @GetMapping("/testReturnCountEvenement")
-    public int returnAllEvent() {
-    	int result = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM evenement", Integer.class);
-    	return result;
-    }
-    
-    
-    @GetMapping("/testReturnEvenementById/{id}")
-    public String returnAllEvent(@PathVariable("id") int id) {
-    	
-    	String query = "SELECT * FROM evenement WHERE uid = ?";
-    	/*
-    	Evenement employee = jdbcTemplate.queryForObject(
-    	  query, 
-    	  new Object[] { id },
-    	  new EvenementRowMapper()
-    	);
-    	*/
-    	try 
-    	{
-        	//jdbcTemplate.queryForObject("select * from student_id = ?", studentRowMapper, studentId)
-        	Evenement event = jdbcTemplate.queryForObject(query, new EvenementRowMapper(), id);
-
-        	return event.toString();
-    	}catch(Exception ex){
-    		if(ex instanceof EmptyResultDataAccessException) {
-    			return "[]"; //TODO method qui return un json indiquant un retour vide ( {code:200, data: []} )
-    		}else {
-    			return "error";
-    		}
-    		
-    	}
-    	
-    	
-    	
-    	/*
-    	jdbcTemplate.query(
-    	        "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", 
-    	        new Object[] { "Josh" },
-    	        (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-    	    )
-    	*/
-    }
-    
-    
 
     @GetMapping("/{id}")
     public String getOne(@PathVariable("id") int id) {
-        return "Evenement avec l'id : " + id;
+		String query = "SELECT * FROM evenement WHERE idevenement = ?";
+		Evenement event = jdbcTemplate.queryForObject(query, new EvenementRowMapper(), id);
+        return new Gson().toJson(event);
     }
     
-    
-
     @GetMapping("/getByDate")
     public String getByDate(@RequestParam("date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date){
-        return "Liste des events pour la date :"+ date.toString(); 
+		String query = "select * from evenement WHERE datedebut < ? AND datefin > ?";
+		List<Evenement> events = jdbcTemplate.query(query, new EvenementRowMapper(), date, date);
+        return new Gson().toJson(events); 
     }
-
+	
+/*
     @GetMapping("/getArea")
     public String getArea(@RequestParam("long") double longitude, @RequestParam("lat") double latitude ){
-        return "Liste des events pour la position :"+ longitude + "    "+ latitude; 
+		String query = "select * from evenement WHERE datedebut < ? AND datefin > ?";
+		List<Evenement> events = jdbcTemplate.query(query, new EvenementRowMapper(), longitude, latitude);
+        return new Gson().toJson(events); 
     }
-    
+*/
 }
 
--- MyEventusApi/src/main/java/model/EvenementRowMapper.java
+++ MyEventusApi/src/main/java/model/EvenementRowMapper.java
@@ -11,12 +11,26 @@ public class EvenementRowMapper implements RowMapper<Evenement> {
     public Evenement mapRow(ResultSet rs, int rowNum) throws SQLException {
 		Evenement event = new Evenement();
 
+        event.setIdEvenement(rs.getInt("idevenement"));
         event.setUid(rs.getInt("uid"));
+        event.setLien(rs.getString("lien"));
+        event.setInformation_Pratiques(rs.getString("informations_pratiques"));
+        event.setImage(rs.getString("image"));
+        event.setLangue(rs.getString("langue"));
         event.setTitre(rs.getString("titre"));
-        //employee.setFirstName(rs.getString("FIRST_NAME"));
-        //employee.setLastName(rs.getString("LAST_NAME"));
-        //employee.setAddress(rs.getString("ADDRESS"));
-
+        event.setDescription(rs.getString("description"));
+        event.setMots_cles(rs.getString("mots_cles"));
+        event.setLatlong(rs.getString("latlon"));
+        event.setPlace(rs.getString("place"));
+        event.setAdresse(rs.getString("adresse"));
+        event.setDepartement(rs.getString("departement"));
+        event.setRegion(rs.getString("region"));
+        event.setVille(rs.getString("ville"));
+        event.setDateDebut(rs.getDate("dateDebut"));
+        event.setDateFin(rs.getDate("dateFin"));
+        event.setTimeTable(rs.getString("timeTable"));
+        event.setTarif(rs.getString("tarif"));
+        event.setDateMiseAJour(rs.getDate("dateMiseAJour"));
         return event;
     }
 }
